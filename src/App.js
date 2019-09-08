200badf7aaedc7aa0e1cc117e5dc1c855ee336f6
Ivan Branets <ivan.branets@gmail.com>
2019-09-08 14:25:21 +0300

    Added initial markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Slider, List, ListItem, Typography } from '@material-ui/core';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FolderIcon from '@material-ui/icons/Folder';
+import data from './data';
 import './App.css';
 
 function App() {
+  const marks = [
+    {
+      value: 0,
+      label: '0°C',
+    },
+    {
+      value: 20,
+      label: '20°C',
+    },
+    {
+      value: 37,
+      label: '37°C',
+    },
+    {
+      value: 100,
+      label: '100°C',
+    },
+  ];
+
   return (
-    <div className="App">
+    <div className="app">
+      <Slider
+        defaultValue={30}
+        valueLabelDisplay="on"
+        min={0}
+        max={1000}
+
+      />
+      <Typography variant="caption">
+        5 items of {numberWithCommas(data.objects.length)} total found
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary="Single-line item"
+            secondary="5Mb"
+          />
+        </ListItem>
+      </List>
     </div>
   );
+}
+
+function numberWithCommas(value) {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 export default App;
