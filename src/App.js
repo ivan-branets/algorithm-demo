2cb940564dbaadb6658b761de7cc16fbe5898f79
Ivan Branets <ivan.branets@gmail.com>
2019-09-08 16:39:40 +0300

    Added react window

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Slider, List, ListItem, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { FixedSizeList } from 'react-window';
+import { Slider, ListItem, Typography } from '@material-ui/core';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import FolderIcon from '@material-ui/icons/Folder';
@@ -31,34 +33,55 @@ function App() {
         min={0}
         max={1000}
         onChange={(event, value) => {
-          const found = findFoldersSlow(value);
+          const found = findFoldersFast(value);
           setFoundFolders(found);
         }}
       />
       <Typography variant="caption">
         {foundFolders.length} items of {numberWithCommas(folders.length)} total found
       </Typography>
-      <List>
-        {
-          foundFolders.map(({name, size}, index) =>
-            <ListItem key={index} dense>
-              <ListItemIcon>
-                <FolderIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={name}
-                secondary={`${size}Mb`}
-              />
-            </ListItem>
-          )
-        }
-      </List>
+      <FixedSizeList itemData={foundFolders} height={200} width={360} itemSize={50} itemCount={foundFolders.length}>
+        {Row}
+      </FixedSizeList>
     </div>
   );
 }
 
+function Row(props) {
+  const { data, index, style } = props;
+  const { name, size } = data[index];
+
+  return <ListItem style={style} key={index}>
+    <ListItemIcon>
+      <FolderIcon />
+    </ListItemIcon>
+    <ListItemText
+      primary={name}
+      secondary={`${size}Mb`}
+    />
+  </ListItem>
+}
+
+Row.propTypes = {
+  index: PropTypes.number.isRequired,
+  style: PropTypes.object.isRequired,
+};
+
 function findFoldersSlow(size) {
-  return folders.filter(item => item.size === size);
+  return folders.filter(folder => folder.size === size);
+}
+
+const map = folders.reduce((result, folder) => {
+  if (result[folder.size] === undefined) {
+    result[folder.size] = [];
+  }
+
+  result[folder.size].push(folder);
+  return result;
+}, {});
+
+function findFoldersFast(size) {
+  return map[size] || [];
 }
 
 function numberWithCommas(value) {
